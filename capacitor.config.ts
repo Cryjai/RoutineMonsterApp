95a5ac76ca35b51340eac5e4c8f27469b6c2f09c
Acry Chak <130869032+Cryjai@users.noreply.github.com>
2026-05-08 00:36:42 +0800

    imported code from vs code

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.acry.routinemonsters',
+  appName: 'Routine Monsters',
+  webDir: 'dist/public'
+};
+
+export default config;
